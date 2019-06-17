6bdb5156a22995cee9ae8b92c152e22fe9d6cce4
Michael Roberts <mroberts@MR-MacBook-Pro.local>
2019-06-17 14:39:12 -0400

    updates to setup.py

--- setup.py
+++ setup.py
@@ -8,26 +8,23 @@ from setuptools import setup
 
 setup(
     name='secretsharing',
-    version='0.2.6',
-    url='https://github.com/onenameio/secret-sharing',
+    version='0.3.7',
+    url='https://github.com/mroberts/secret-sharing',
     license='MIT',
-    author='Halfmoon Labs',
-    author_email='hello@halfmoon.io',
+    author='mroberts',
+    author_email='',
     description=("Tools for sharing secrets (like Bitcoin private keys), "
                  "using shamir's secret sharing scheme."),
     packages=[
         'secretsharing',
     ],
     zip_safe=False,
-    install_requires=[
-        'six',
-        'utilitybelt',
-    ],
+    install_requires=[],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
     ],
 )
